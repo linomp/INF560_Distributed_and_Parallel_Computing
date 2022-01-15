d8715f3297d70f406e78b9094f55c7416e14bd65
linomp <lm2p@hotmail.es>
2022-01-15 05:27:33 +0100

    lab2 part 1

--- lab2/hello2.c
+++ lab2/hello2.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <limits.h>
-
 /* MPI function signatures */
 #include <mpi.h>
 
 int main(int argc, char **argv)
 {
-    int N, me;
-    char hostname[HOST_NAME_MAX + 1];
+    int N, me, t;
+    int buff;
+
+    MPI_Status status;
 
     /* Initialization of MPI */
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &N);
     MPI_Comm_rank(MPI_COMM_WORLD, &me);
 
-    gethostname(hostname, HOST_NAME_MAX + 1);
+    if (me == 0)
+    {
+        // wait for N-1 messages
+        for (t = 1; t < N; t++)
+        {
+            MPI_Recv(&buff, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        }
+        printf("Hello World with %d ready tasks\n", N);
+    }
+    else
+    {
+        // send message confirming task is ready
+        MPI_Send(&buff, 1, MPI_DOUBLE, 0, me, MPI_COMM_WORLD);
+    }
 
-    printf("Hello World from task %d out of %d on %s\n", me, N, hostname);
     MPI_Finalize();
     return 0;
 }
